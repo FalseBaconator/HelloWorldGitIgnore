71a22cbbac36cb52ab2acdaac9d9a5c8612012e6
FalseBaconator <113189775+FalseBaconator@users.noreply.github.com>
2022-09-12 11:37:22 -0300

    Initial commit

---
+++ HelloWorldGitIgnore/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldGitIgnore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World but this time using the right Git Ignore");
+            Console.WriteLine("Hopefully this is done correctly this time");
+            Console.ReadKey(false);
+        }
+    }
+}
